355084f4911a9b2160852991a110a5e503380770
chris <christopher@imamonster.org>
2016-03-06 01:14:21 +0000

    added jquery

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 	http.HandleFunc("/", handler.IndexHandler)
 	http.HandleFunc("/scan", handler.ScanHandler)
 	http.HandleFunc("/settings", handler.SettingsHandler)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.ListenAndServe(":8080", nil)
 }
